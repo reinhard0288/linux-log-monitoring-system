d23f854483499f5d9bc3198cab90a206def97366
reinhard0288 <reinhard0288@gmail.com>
2026-05-09 13:31:13 +0530

    Added real email alert system

--- monitor.py
+++ monitor.py
@@ -1,5 +1,18 @@
+import smtplib
+from email.message import EmailMessage
 def send_alert():
-    print("EMAIL SEND TO ADMIN")
+    sender_email = "kingreinhard8877@gmail.com"
+    app_password = "muarpshriynnzmvs"
+    receiver_email = "siripakasaikumar02@gmail.com"
+    msg = EmailMessage()
+    msg["Subject"] = "Security Alert: Suspicious Login Activity"
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg.set_content("Alert! Multiple failed login attempts detcted on the server.")
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(sender_email,app_password)
+        smtp.send_message(msg)
+    print("REAL EMAIL ALERT SENT TO ADMIN")
 print("Linux Server LOG MONITORING STARTED")
 log_file = "sample_log.txt"
 with open (log_file,"r") as file:
